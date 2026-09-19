1217ab3c4f3cd1f2d9a6b3985959c8f582bcf0b6
divineoff <denyssamoiliuk@gmail.com>
2026-09-19 23:26:56 +0300

    Commit 3: Updated output text

--- Lab02/Lab02.cpp
+++ Lab02/Lab02.cpp
@@ -7,10 +7,11 @@ int main()
 	double alpha, z1, z2;
 	cout << "alpha = "; cin >> alpha;
 	z1 = (sin(alpha) + sin(5 * alpha) - sin(3 * alpha)) / (cos(alpha) - cos(3 * alpha) + cos(5 * alpha));
-	// z2 = tan(3 * alpha);
+	z2 = tan(3 * alpha);
 	cout << endl;
+	cout << "Commit 3 version!!!" << endl;
 	cout << "z1 = " << z1 << endl;
-	// cout << "z2 = " << z2 << endl;
+	cout << "z2 = " << z2 << endl;
 
 	return 0;
 }
