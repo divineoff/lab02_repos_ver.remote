6d8c06c92b30fcfa0bdaf3d69d5f15e7d2af1449
divineoff <denyssamoiliuk@gmail.com>
2026-09-19 23:24:29 +0300

    Commit 2: z2 commented

--- Lab02/Lab02.cpp
+++ Lab02/Lab02.cpp
@@ -7,10 +7,10 @@ int main()
 	double alpha, z1, z2;
 	cout << "alpha = "; cin >> alpha;
 	z1 = (sin(alpha) + sin(5 * alpha) - sin(3 * alpha)) / (cos(alpha) - cos(3 * alpha) + cos(5 * alpha));
-	z2 = tan(3 * alpha);
+	// z2 = tan(3 * alpha);
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
-	cout << "z2 = " << z2 << endl;
+	// cout << "z2 = " << z2 << endl;
 
 	return 0;
 }
