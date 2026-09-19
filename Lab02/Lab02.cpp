3f39d970bcd91e419fa4d7217058fd8c6fd336c8
divineoff <denyssamoiliuk@gmail.com>
2026-09-19 23:22:47 +0300

    Commit 1: Initial project structure

---
+++ Lab02/Lab02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double alpha, z1, z2;
+	cout << "alpha = "; cin >> alpha;
+	z1 = (sin(alpha) + sin(5 * alpha) - sin(3 * alpha)) / (cos(alpha) - cos(3 * alpha) + cos(5 * alpha));
+	z2 = tan(3 * alpha);
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+	return 0;
+}
